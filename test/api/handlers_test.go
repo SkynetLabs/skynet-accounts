421bb14fbb064749b58d3bb68419f031dfaa13b9
Ivaylo Novakov <inovakov@gmail.com>
2021-09-28 12:35:45 +0200

    Add a test for user creation and login.

--- test/api/handlers_test.go
+++ test/api/handlers_test.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"encoding/hex"
 	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/NebulousLabs/skynet-accounts/test"
+	"gitlab.com/NebulousLabs/fastrand"
 )
 
 type subtest struct {
@@ -23,7 +26,8 @@ func TestHandlers(t *testing.T) {
 
 	// Specify subtests to run
 	tests := []subtest{
-		{Name: "SingleFileRegular", Test: testHandlerHealth},
+		{Name: "health", Test: testHandlerHealthGET},
+		{Name: "createUser", Test: testHandlerUserPOST},
 	}
 
 	// Run subtests
@@ -34,8 +38,41 @@ func TestHandlers(t *testing.T) {
 	}
 }
 
-func testHandlerHealth(t *testing.T, at *test.AccountsTester) {
-	_, _, b, err := at.Get("/health", map[string]string{})
+// testHandlerUserPOST tests user creation and login.
+func testHandlerUserPOST(t *testing.T, at *test.AccountsTester) {
+	name := strings.ReplaceAll(t.Name(), "/", "_")
+	params := map[string]string{
+		"email":    name + "@siasky.net",
+		"password": hex.EncodeToString(fastrand.Bytes(16)),
+	}
+	// Create a user.
+	_, _, err := at.Post("/user", map[string]string{}, params)
+	if err != nil {
+		t.Fatalf("User creation failed. Error %s", err.Error())
+	}
+	// Clean up the user after the test.
+	defer func() {
+		u, err := at.DB.UserByEmail(at.Ctx, params["email"], false)
+		if err != nil {
+			t.Logf("Error while cleaning up user: %s", err.Error())
+			return
+		}
+		err = at.DB.UserDelete(at.Ctx, u)
+		if err != nil {
+			t.Logf("Error while cleaning up user: %s", err.Error())
+			return
+		}
+	}()
+	// Log in with that user in order to make sure it exists.
+	_, _, err = at.Post("/login", map[string]string{}, params)
+	if err != nil {
+		t.Fatalf("Login failed. Error %s", err.Error())
+	}
+}
+
+// testHandlerHealthGET tests the /health handler.
+func testHandlerHealthGET(t *testing.T, at *test.AccountsTester) {
+	_, b, err := at.Get("/health", map[string]string{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -46,6 +83,8 @@ func testHandlerHealth(t *testing.T, at *test.AccountsTester) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	// DBAlive should never be false because if we couldn't reach the DB, we
+	// wouldn't have made it this far in the test.
 	if !status.DBAlive {
 		t.Fatal("DB down.")
 	}
--- test/tester.go
+++ test/tester.go
@@ -3,11 +3,11 @@ package test
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/NebulousLabs/skynet-accounts/api"
 	"github.com/NebulousLabs/skynet-accounts/database"
@@ -28,11 +28,22 @@ type (
 	// instance of the service and provides simplified ways to call the handlers.
 	AccountsTester struct {
 		cancel context.CancelFunc
-		ctx    context.Context // TODO Do I need this?
-		db     *database.DB    // TODO This will probably come handy for inspecting the DB.
-		logger *logrus.Logger  // TODO Do I need this?
+		Ctx    context.Context
+		DB     *database.DB
+		Logger *logrus.Logger
 	}
 )
+
+// ExtractCookie is a helper method which extracts the login cookie from a
+// response, so we can use it with future requests while testing.
+func ExtractCookie(r *http.Response) *http.Cookie {
+	for _, c := range r.Cookies() {
+		if strings.HasPrefix(api.CookieName, c.String()) {
+			return c
+		}
+	}
+	return nil
+}
 
 // NewAccountsTester creates and starts a new AccountsTester service.
 // Use the Shutdown method for a graceful shutdown.
@@ -91,56 +102,52 @@ func NewAccountsTester() (*AccountsTester, error) {
 		select {
 		case <-ctx.Done():
 			println("*** Shutting down test server") // TODO DEBUG
-			_ = srv.Shutdown(context.TODO())         // TODO I can't pass ctx here, as it's closed now.
+			_ = srv.Shutdown(context.TODO())         // TODO I can't pass Ctx here, as it's closed now.
 		}
 	}()
 
 	return &AccountsTester{
 		cancel: cancel,
-		ctx:    ctx,
-		db:     db,
+		Ctx:    ctx,
+		DB:     db,
 		logger: logger,
 	}, nil
 }
 
 // Delete executes a DELETE request against the test service.
-func (at *AccountsTester) Delete(endpoint string, params map[string]string) (status int, headers http.Header, body []byte, err error) {
+func (at *AccountsTester) Delete(endpoint string, params map[string]string) (r *http.Response, body []byte, err error) {
 	req, err := http.NewRequest(http.MethodDelete, testPortalAddr+":"+testPortalPort+endpoint+"?"+encodeValues(params), bytes.NewBuffer([]byte{}))
 	if err != nil {
 		return
 	}
 	client := &http.Client{}
-	r, err := client.Do(req)
+	r, err = client.Do(req)
 	if err != nil {
 		return
 	}
-	body, err = ioutil.ReadAll(r.Body)
-	return r.StatusCode, r.Header, body, err
+	return processResponse(r)
 }
 
 // Get executes a GET request against the test service.
-func (at *AccountsTester) Get(endpoint string, params map[string]string) (status int, headers http.Header, body []byte, err error) {
-	r, err := http.Get(testPortalAddr + ":" + testPortalPort + endpoint + "?" + encodeValues(params))
+func (at *AccountsTester) Get(endpoint string, params map[string]string) (r *http.Response, body []byte, err error) {
+	r, err = http.Get(testPortalAddr + ":" + testPortalPort + endpoint + "?" + encodeValues(params))
 	if err != nil {
 		return
 	}
-	body, err = ioutil.ReadAll(r.Body)
-	return r.StatusCode, r.Header, body, err
+	return processResponse(r)
 }
 
 // Post executes a POST request against the test service.
-func (at *AccountsTester) Post(endpoint string, params map[string]string, postParams map[string]string) (status int, headers http.Header, body []byte, err error) {
-	reqBody, err := json.Marshal(postParams)
+func (at *AccountsTester) Post(endpoint string, params map[string]string, postParams map[string]string) (r *http.Response, body []byte, err error) {
+	vals := url.Values{}
+	for k, v := range postParams {
+		vals[k] = append(vals[k], v)
+	}
+	r, err = http.PostForm(testPortalAddr+":"+testPortalPort+endpoint+"?"+encodeValues(params), vals)
 	if err != nil {
 		return
 	}
-	// TODO Should we use PostForm instead?
-	r, err := http.Post(testPortalAddr+":"+testPortalPort+endpoint+"?"+encodeValues(params), "application/json", bytes.NewBuffer(reqBody))
-	if err != nil {
-		return
-	}
-	body, err = ioutil.ReadAll(r.Body)
-	return r.StatusCode, r.Header, body, err
+	return processResponse(r)
 }
 
 // Shutdown performs a graceful shutdown of the AccountsTester service.
@@ -155,4 +162,17 @@ func encodeValues(values map[string]string) string {
 		v[key] = append(v[key], values[key])
 	}
 	return v.Encode()
+}
+
+// processResponse is a helper method which extracts the body from the response
+// and handles non-OK status codes.
+func processResponse(r *http.Response) (*http.Response, []byte, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	_ = r.Body.Close()
+	// For convenience, whenever we have a non-OK status we'll wrap it in an
+	// error.
+	if r.StatusCode < 200 || r.StatusCode > 299 {
+		err = errors.Compose(err, errors.New(r.Status))
+	}
+	return r, body, err
 }
