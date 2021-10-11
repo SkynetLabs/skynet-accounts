c07d3f70ea9371587a01e248c92256a370ef7773
Ivaylo Novakov <inovakov@gmail.com>
2021-10-11 14:03:36 +0200

    Cookie key size in const.

--- api/cookie.go
+++ api/cookie.go
@@ -9,7 +9,6 @@ import (
 	"github.com/NebulousLabs/skynet-accounts/build"
 	"github.com/gorilla/securecookie"
 	"github.com/joho/godotenv"
-	"gitlab.com/NebulousLabs/fastrand"
 )
 
 const (
@@ -25,6 +24,9 @@ const (
 	// envCookieEncKey holds the name of the env var which holds the key we use
 	// to encrypt cookies.
 	envCookieEncKey = "COOKIE_ENC_KEY"
+	// secureCookieKeySize defines the size of the hash key and encryption key
+	// we want to use for setting our cookie.
+	secureCookieKeySize = 32
 )
 
 var (
@@ -34,18 +36,18 @@ var (
 		encKeyStr := os.Getenv(envCookieEncKey)
 		if build.Release == "testing" {
 			if len(hashKeyStr) < 32 {
-				hashKeyStr = string(fastrand.Bytes(32))
+				hashKeyStr = string(securecookie.GenerateRandomKey(secureCookieKeySize))
 			}
 			if len(encKeyStr) < 32 {
-				encKeyStr = string(fastrand.Bytes(32))
+				encKeyStr = string(securecookie.GenerateRandomKey(secureCookieKeySize))
 			}
 		}
-		if len(hashKeyStr) < 32 || len(encKeyStr) < 32 {
+		if len(hashKeyStr) < secureCookieKeySize || len(encKeyStr) < secureCookieKeySize {
 			panic(fmt.Sprintf("Both %s and %s are required environment variables and need to contain at least 32 bytes of hex-encoded entropy. %s, %s", envCookieHashKey, envCookieEncKey, hashKeyStr, encKeyStr))
 		}
 		// These keys need to be *exactly* 16 or 32 bytes long.
-		var hashKey = []byte(hashKeyStr)[:32]
-		var encKey = []byte(encKeyStr)[:32]
+		var hashKey = []byte(hashKeyStr)[:secureCookieKeySize]
+		var encKey = []byte(encKeyStr)[:secureCookieKeySize]
 		return securecookie.New(hashKey, encKey)
 	}()
 )
