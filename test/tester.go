850a9ae01d6ca814aabc842741f429a4fff8861e
Ivaylo Novakov <inovakov@gmail.com>
2022-02-23 17:28:37 +0100

    Deduplicate authentication code.

--- test/tester.go
+++ test/tester.go
@@ -163,18 +163,7 @@ func (at *AccountsTester) Post(endpoint string, params url.Values, bodyParams ur
 		return nil, nil, err
 	}
 	req.Header.Set("Content-Type", "application/json")
-	if at.Cookie != nil {
-		req.Header.Set("Cookie", at.Cookie.String())
-	}
-	if at.Token != "" {
-		req.Header.Set("Authorization", "Bearer "+at.Token)
-	}
-	c := http.Client{}
-	r, err = c.Do(req)
-	if err != nil {
-		return
-	}
-	return processResponse(r)
+	return at.executeRequest(req)
 }
 
 // Put executes a PUT request against the test service.
@@ -217,18 +206,7 @@ func (at *AccountsTester) UserPUT(email, password, stipeID string) (*http.Respon
 	if err != nil {
 		return nil, nil, err
 	}
-	if at.Cookie != nil {
-		req.Header.Set("Cookie", at.Cookie.String())
-	}
-	if at.Token != "" {
-		req.Header.Set("Authorization", "Bearer "+at.Token)
-	}
-	client := http.Client{}
-	r, err := client.Do(req)
-	if err != nil {
-		return nil, nil, err
-	}
-	return processResponse(r)
+	return at.executeRequest(req)
 }
 
 // request is a helper method that puts together and executes an HTTP
@@ -248,6 +226,17 @@ func (at *AccountsTester) request(method string, endpoint string, queryParams ur
 	if err != nil {
 		return nil, nil, err
 	}
+	return at.executeRequest(req)
+}
+
+// executeRequest is a helper method which executes a test request and processes
+// the response by extracting the body from it and handling non-OK status codes.
+//
+// NOTE: The Body of the returned response is already read and closed.
+func (at *AccountsTester) executeRequest(req *http.Request) (*http.Response, []byte, error) {
+	if req == nil {
+		return nil, nil, errors.New("invalid request")
+	}
 	if at.Cookie != nil {
 		req.Header.Set("Cookie", at.Cookie.String())
 	}
@@ -259,14 +248,6 @@ func (at *AccountsTester) request(method string, endpoint string, queryParams ur
 	if err != nil {
 		return nil, nil, err
 	}
-	return processResponse(r)
-}
-
-// processResponse is a helper method which extracts the body from the response
-// and handles non-OK status codes.
-//
-// NOTE: The Body of the returned response is already read and closed.
-func processResponse(r *http.Response) (*http.Response, []byte, error) {
 	body, err := ioutil.ReadAll(r.Body)
 	_ = r.Body.Close()
 	// For convenience, whenever we have a non-OK status we'll wrap it in an
