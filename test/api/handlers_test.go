b9036cf9bd78e9d508bd7209dadeb0a675bf119b
Ivaylo Novakov <inovakov@gmail.com>
2022-03-25 14:22:04 +0100

    Fix a test.

--- test/api/handlers_test.go
+++ test/api/handlers_test.go
@@ -941,12 +941,10 @@ func testTrackingAndStats(t *testing.T, at *test.AccountsTester) {
 	expectedStats.BandwidthUploads += skynet.BandwidthUploadCost(0)
 	expectedStats.RawStorageUsed += skynet.RawStorageUsed(0)
 
-	// Call trackDownload without a cookie. Expect this to succeed.
-	// While we expect this to succeed, it won't be counted towards the user's
-	// quota, so we don't increment the expected stats.
+	// Call trackDownload without a cookie. Expect this to fail.
 	at.ClearCredentials()
 	_, err = at.TrackDownload(skylink.String(), 100)
-	if err != nil {
+	if err == nil {
 		t.Fatal(err)
 	}
 	at.SetCookie(c)
