94ca9b58058e19e30b67c4d4b94f915dd97c01f5
Ivaylo Novakov <inovakov@gmail.com>
2022-04-01 16:01:49 +0200

    Refactor TestCleanName.

--- test/tester_test.go
+++ test/tester_test.go
@@ -4,27 +4,27 @@ import "testing"
 
 // TestCleanName ensures that CleanName works as expected.
 func TestCleanName(t *testing.T) {
-	tests := []struct {
-		name     string
+	tests := map[string]struct {
 		input    string
 		expected string
 	}{
-		{name: "valid name", input: "valid_name", expected: "valid_name"},
-		{name: "with space", input: "with space", expected: "with_space"},
+		"valid name": {input: "valid_name", expected: "valid_name"},
+		"with space": {input: "with space", expected: "with_space"},
 		// The list of bad characters is: /\. "$*<>:|?
 		// See https://docs.mongodb.com/manual/reference/limits/#naming-restrictions
-		{name: "various bad characters", input: "/\\. \"$*<>:|?", expected: "____________"},
-		{
-			name:     "long name",
+		"various bad characters": {input: "/\\. \"$*<>:|?", expected: "____________"},
+		"long name": {
 			input:    "this_is_a_very_long_name_and_it_will_be_trimmed_to_sixty_four_characters",
 			expected: "this_is_a_very_long_name_and_it_will_be_trimmed_to_sixty_four_characters"[:64],
 		},
 	}
 
-	for _, tt := range tests {
-		out := CleanName(tt.input)
-		if out != tt.expected {
-			t.Errorf("Test '%s' failed. Expected '%s', got '%s'", tt.name, tt.expected, out)
-		}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			out := CleanName(tt.input)
+			if out != tt.expected {
+				t.Errorf("Test '%s' failed. Expected '%s', got '%s'", name, tt.expected, out)
+			}
+		})
 	}
 }
