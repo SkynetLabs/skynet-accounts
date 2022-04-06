ccbe62364eefb31a7ba251cfbfffeac405c64959
Ivaylo Novakov <inovakov@gmail.com>
2022-04-06 16:41:04 +0200

    Alphabetize methods.

--- api/stripe.go
+++ api/stripe.go
@@ -75,99 +75,75 @@ type (
 	}
 )
 
-// stripeWebhookPOST handles various events issued by Stripe.
-// See https://stripe.com/docs/api/events/types
-func (api *API) stripeWebhookPOST(_ *database.User, w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	api.staticLogger.Tracef("Webhook request: %+v", req)
-	event, code, err := readStripeEvent(w, req)
+// processStripeSub reads the information about the user's subscription and
+// adjusts the user's record accordingly.
+func (api *API) processStripeSub(ctx context.Context, s *stripe.Subscription) error {
+	api.staticLogger.Traceln("Processing subscription:", s.ID)
+	u, err := api.staticDB.UserByStripeID(ctx, s.Customer.ID)
 	if err != nil {
-		api.WriteError(w, err, code)
-		return
+		errMsg := fmt.Sprintf("failed to fetch user from DB for customer id %s", s.Customer.ID)
+		return errors.AddContext(err, errMsg)
 	}
-	api.staticLogger.Tracef("Webhook event: %+v", event)
-
-	// Here we handle the entire class of subscription events.
-	// https://stripe.com/docs/billing/subscriptions/overview#build-your-own-handling-for-recurring-charge-failures
-	// https://stripe.com/docs/api/subscriptions/object
-	if strings.Contains(event.Type, "customer.subscription") {
-		var s stripe.Subscription
-		err = json.Unmarshal(event.Data.Raw, &s)
-		if err != nil {
-			api.staticLogger.Warningln("Webhook: Failed to parse event. Error: ", err, "\nEvent: ", string(event.Data.Raw))
-			api.WriteError(w, err, http.StatusBadRequest)
-			return
-		}
-		err = api.processStripeSub(req.Context(), &s)
-		if err != nil {
-			api.staticLogger.Debugln("Webhook: Failed to process sub:", err)
-			api.WriteError(w, err, http.StatusInternalServerError)
-			return
-		}
-		api.WriteSuccess(w)
-		return
-	}
-
-	// Here we handle the entire class of subscription_schedule events.
-	// See https://stripe.com/docs/api/subscription_schedules/object
-	if strings.Contains(event.Type, "subscription_schedule") {
-		var hasSub struct {
-			Sub string `json:"subscription"`
-		}
-		err = json.Unmarshal(event.Data.Raw, &hasSub)
-		if err != nil {
-			api.staticLogger.Warningln("Webhook: Failed to parse event. Error: ", err, "\nEvent: ", string(event.Data.Raw))
-			api.WriteError(w, err, http.StatusBadRequest)
-			return
-		}
-		if hasSub.Sub == "" {
-			api.staticLogger.Debugln("Webhook: Event doesn't refer to a subscription.")
-			api.WriteSuccess(w)
-			return
-		}
-		// Check the details about this subscription:
-		s, err := sub.Get(hasSub.Sub, nil)
-		if err != nil {
-			api.staticLogger.Debugln("Webhook: Failed to fetch sub:", err)
-			api.WriteError(w, err, http.StatusInternalServerError)
-			return
-		}
-		err = api.processStripeSub(req.Context(), s)
-		if err != nil {
-			api.staticLogger.Debugln("Webhook: Failed to process sub:", err)
-			api.WriteError(w, err, http.StatusInternalServerError)
-			return
+	// Get all active subscriptions for this customer. There should be only one
+	// (or none) but we'd better check.
+	it := sub.List(&stripe.SubscriptionListParams{
+		Customer: s.Customer.ID,
+		Status:   string(stripe.SubscriptionStatusActive),
+	})
+	// Pick the latest active plan and set the user's tier based on that.
+	subs := it.SubscriptionList().Data
+	var mostRecentSub *stripe.Subscription
+	for _, subsc := range subs {
+		if mostRecentSub == nil || subsc.Created > mostRecentSub.Created {
+			mostRecentSub = subsc
 		}
 	}
-
-	api.WriteSuccess(w)
-}
-
-// stripePricesGET returns a list of plans and prices.
-func (api *API) stripePricesGET(_ *database.User, w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
-	var sPrices []stripePrice
-	params := &stripe.PriceListParams{Active: &True}
-	params.AddExpand("data.product")
-	params.Filters.AddFilter("limit", "", "1000")
-	i := price.List(params)
-	for i.Next() {
-		p := i.Price()
-		if !p.Active {
+	if mostRecentSub == nil {
+		// No active sub, set the default values.
+		u.Tier = database.TierFree
+		u.SubscribedUntil = time.Time{}
+		u.SubscriptionStatus = ""
+		u.SubscriptionCancelAt = time.Time{}
+		u.SubscriptionCancelAtPeriodEnd = false
+	} else {
+		// It seems weird that the Plan.ID is actually a price id but this
+		// is what we get from Stripe.
+		u.Tier = stripePrices()[mostRecentSub.Plan.ID]
+		u.SubscribedUntil = time.Unix(mostRecentSub.CurrentPeriodEnd, 0).UTC().Truncate(time.Millisecond)
+		u.SubscriptionStatus = string(mostRecentSub.Status)
+		u.SubscriptionCancelAt = time.Unix(mostRecentSub.CancelAt, 0).UTC().Truncate(time.Millisecond)
+		u.SubscriptionCancelAtPeriodEnd = mostRecentSub.CancelAtPeriodEnd
+	}
+	// Cancel all subs aside from the latest one.
+	p := stripe.SubscriptionCancelParams{
+		Params: stripe.Params{
+			StripeAccount: &s.Customer.ID,
+		},
+		InvoiceNow: &True,
+		Prorate:    &True,
+	}
+	for _, subsc := range subs {
+		if subsc == nil || (mostRecentSub != nil && subsc.ID == mostRecentSub.ID) {
 			continue
 		}
-		sp := stripePrice{
-			ID:          p.ID,
-			Name:        p.Product.Name,
-			Description: p.Product.Description,
-			Tier:        stripePrices()[p.ID],
-			Price:       float64(p.UnitAmount) / 100,
-			Currency:    string(p.Currency),
-			StripeID:    p.ID,
-			ProductID:   p.Product.ID,
-			LiveMode:    p.Livemode,
+		if subsc.ID == "" {
+			api.staticLogger.Warnf("Empty subscription ID! User ID '%s', Stripe ID '%s', subscription object '%+v'", u.ID.Hex(), u.StripeID, subs)
+			continue
 		}
-		sPrices = append(sPrices, sp)
+		subsc, err = sub.Cancel(subsc.ID, &p)
+		if err != nil {
+			api.staticLogger.Warnf("Failed to cancel sub with id '%s' for user '%s' with Stripe customer id '%s'. Error: '%s'", subsc.ID, u.ID.Hex(), s.Customer.ID, err.Error())
+		} else {
+			api.staticLogger.Tracef("Successfully cancelled sub with id '%s' for user '%s' with Stripe customer id '%s'.", subsc.ID, u.ID.Hex(), s.Customer.ID)
+		}
 	}
-	api.WriteJSON(w, sPrices)
+	err = api.staticDB.UserSave(ctx, u)
+	if err == nil {
+		api.staticLogger.Tracef("Subscribed user id '%s', tier %d, until %s.", u.ID, u.Tier, u.SubscribedUntil.String())
+	}
+	// Re-set the tier cache for this user, in case their tier changed.
+	api.staticUserTierCache.Set(u.Sub, u)
+	return err
 }
 
 // stripeBillingPOST creates a new billing session for the user and redirects
@@ -248,6 +224,49 @@ func (api *API) stripeCheckoutPOST(u *database.User, w http.ResponseWriter, req 
 	api.WriteJSON(w, response)
 }
 
+// stripeCreateCustomer creates a Stripe customer record for this user and
+// updates the user in the database.
+func (api *API) stripeCreateCustomer(ctx context.Context, u *database.User) (string, error) {
+	cus, err := customer.New(&stripe.CustomerParams{})
+	if err != nil {
+		return "", errors.AddContext(err, "failed to create Stripe customer")
+	}
+	stripeID := cus.ID
+	err = api.staticDB.UserSetStripeID(ctx, u, stripeID)
+	if err != nil {
+		return "", errors.AddContext(err, "failed to save user's StripeID")
+	}
+	return stripeID, nil
+}
+
+// stripePricesGET returns a list of plans and prices.
+func (api *API) stripePricesGET(_ *database.User, w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	var sPrices []stripePrice
+	params := &stripe.PriceListParams{Active: &True}
+	params.AddExpand("data.product")
+	params.Filters.AddFilter("limit", "", "1000")
+	i := price.List(params)
+	for i.Next() {
+		p := i.Price()
+		if !p.Active {
+			continue
+		}
+		sp := stripePrice{
+			ID:          p.ID,
+			Name:        p.Product.Name,
+			Description: p.Product.Description,
+			Tier:        stripePrices()[p.ID],
+			Price:       float64(p.UnitAmount) / 100,
+			Currency:    string(p.Currency),
+			StripeID:    p.ID,
+			ProductID:   p.Product.ID,
+			LiveMode:    p.Livemode,
+		}
+		sPrices = append(sPrices, sp)
+	}
+	api.WriteJSON(w, sPrices)
+}
+
 // stripeSubscriptionsGET fetches the first Stripe subscription that belongs to
 // the user.
 func (api *API) stripeSubscriptionsGET(u *database.User, w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
@@ -280,19 +299,71 @@ func (api *API) stripeSubscriptionsGET(u *database.User, w http.ResponseWriter, 
 	api.WriteJSON(w, cus.Subscriptions.Data[0])
 }
 
-// stripeCreateCustomer creates a Stripe customer record for this user and
-// updates the user in the database.
-func (api *API) stripeCreateCustomer(ctx context.Context, u *database.User) (string, error) {
-	cus, err := customer.New(&stripe.CustomerParams{})
+// stripeWebhookPOST handles various events issued by Stripe.
+// See https://stripe.com/docs/api/events/types
+func (api *API) stripeWebhookPOST(_ *database.User, w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	api.staticLogger.Tracef("Webhook request: %+v", req)
+	event, code, err := readStripeEvent(w, req)
 	if err != nil {
-		return "", errors.AddContext(err, "failed to create Stripe customer")
+		api.WriteError(w, err, code)
+		return
 	}
-	stripeID := cus.ID
-	err = api.staticDB.UserSetStripeID(ctx, u, stripeID)
-	if err != nil {
-		return "", errors.AddContext(err, "failed to save user's StripeID")
+	api.staticLogger.Tracef("Webhook event: %+v", event)
+
+	// Here we handle the entire class of subscription events.
+	// https://stripe.com/docs/billing/subscriptions/overview#build-your-own-handling-for-recurring-charge-failures
+	// https://stripe.com/docs/api/subscriptions/object
+	if strings.Contains(event.Type, "customer.subscription") {
+		var s stripe.Subscription
+		err = json.Unmarshal(event.Data.Raw, &s)
+		if err != nil {
+			api.staticLogger.Warningln("Webhook: Failed to parse event. Error: ", err, "\nEvent: ", string(event.Data.Raw))
+			api.WriteError(w, err, http.StatusBadRequest)
+			return
+		}
+		err = api.processStripeSub(req.Context(), &s)
+		if err != nil {
+			api.staticLogger.Debugln("Webhook: Failed to process sub:", err)
+			api.WriteError(w, err, http.StatusInternalServerError)
+			return
+		}
+		api.WriteSuccess(w)
+		return
 	}
-	return stripeID, nil
+
+	// Here we handle the entire class of subscription_schedule events.
+	// See https://stripe.com/docs/api/subscription_schedules/object
+	if strings.Contains(event.Type, "subscription_schedule") {
+		var hasSub struct {
+			Sub string `json:"subscription"`
+		}
+		err = json.Unmarshal(event.Data.Raw, &hasSub)
+		if err != nil {
+			api.staticLogger.Warningln("Webhook: Failed to parse event. Error: ", err, "\nEvent: ", string(event.Data.Raw))
+			api.WriteError(w, err, http.StatusBadRequest)
+			return
+		}
+		if hasSub.Sub == "" {
+			api.staticLogger.Debugln("Webhook: Event doesn't refer to a subscription.")
+			api.WriteSuccess(w)
+			return
+		}
+		// Check the details about this subscription:
+		s, err := sub.Get(hasSub.Sub, nil)
+		if err != nil {
+			api.staticLogger.Debugln("Webhook: Failed to fetch sub:", err)
+			api.WriteError(w, err, http.StatusInternalServerError)
+			return
+		}
+		err = api.processStripeSub(req.Context(), s)
+		if err != nil {
+			api.staticLogger.Debugln("Webhook: Failed to process sub:", err)
+			api.WriteError(w, err, http.StatusInternalServerError)
+			return
+		}
+	}
+
+	api.WriteSuccess(w)
 }
 
 // readStripeEvent reads the event from the request body and verifies its
@@ -310,77 +381,6 @@ func readStripeEvent(w http.ResponseWriter, req *http.Request) (*stripe.Event, i
 		return nil, http.StatusBadRequest, err
 	}
 	return &event, http.StatusOK, nil
-}
-
-// processStripeSub reads the information about the user's subscription and
-// adjusts the user's record accordingly.
-func (api *API) processStripeSub(ctx context.Context, s *stripe.Subscription) error {
-	api.staticLogger.Traceln("Processing subscription:", s.ID)
-	u, err := api.staticDB.UserByStripeID(ctx, s.Customer.ID)
-	if err != nil {
-		errMsg := fmt.Sprintf("failed to fetch user from DB for customer id %s", s.Customer.ID)
-		return errors.AddContext(err, errMsg)
-	}
-	// Get all active subscriptions for this customer. There should be only one
-	// (or none) but we'd better check.
-	it := sub.List(&stripe.SubscriptionListParams{
-		Customer: s.Customer.ID,
-		Status:   string(stripe.SubscriptionStatusActive),
-	})
-	// Pick the latest active plan and set the user's tier based on that.
-	subs := it.SubscriptionList().Data
-	var mostRecentSub *stripe.Subscription
-	for _, subsc := range subs {
-		if mostRecentSub == nil || subsc.Created > mostRecentSub.Created {
-			mostRecentSub = subsc
-		}
-	}
-	if mostRecentSub == nil {
-		// No active sub, set the default values.
-		u.Tier = database.TierFree
-		u.SubscribedUntil = time.Time{}
-		u.SubscriptionStatus = ""
-		u.SubscriptionCancelAt = time.Time{}
-		u.SubscriptionCancelAtPeriodEnd = false
-	} else {
-		// It seems weird that the Plan.ID is actually a price id but this
-		// is what we get from Stripe.
-		u.Tier = stripePrices()[mostRecentSub.Plan.ID]
-		u.SubscribedUntil = time.Unix(mostRecentSub.CurrentPeriodEnd, 0).UTC().Truncate(time.Millisecond)
-		u.SubscriptionStatus = string(mostRecentSub.Status)
-		u.SubscriptionCancelAt = time.Unix(mostRecentSub.CancelAt, 0).UTC().Truncate(time.Millisecond)
-		u.SubscriptionCancelAtPeriodEnd = mostRecentSub.CancelAtPeriodEnd
-	}
-	// Cancel all subs aside from the latest one.
-	p := stripe.SubscriptionCancelParams{
-		Params: stripe.Params{
-			StripeAccount: &s.Customer.ID,
-		},
-		InvoiceNow: &True,
-		Prorate:    &True,
-	}
-	for _, subsc := range subs {
-		if subsc == nil || (mostRecentSub != nil && subsc.ID == mostRecentSub.ID) {
-			continue
-		}
-		if subsc.ID == "" {
-			api.staticLogger.Warnf("Empty subscription ID! User ID '%s', Stripe ID '%s', subscription object '%+v'", u.ID.Hex(), u.StripeID, subs)
-			continue
-		}
-		subsc, err = sub.Cancel(subsc.ID, &p)
-		if err != nil {
-			api.staticLogger.Warnf("Failed to cancel sub with id '%s' for user '%s' with Stripe customer id '%s'. Error: '%s'", subsc.ID, u.ID.Hex(), s.Customer.ID, err.Error())
-		} else {
-			api.staticLogger.Tracef("Successfully cancelled sub with id '%s' for user '%s' with Stripe customer id '%s'.", subsc.ID, u.ID.Hex(), s.Customer.ID)
-		}
-	}
-	err = api.staticDB.UserSave(ctx, u)
-	if err == nil {
-		api.staticLogger.Tracef("Subscribed user id '%s', tier %d, until %s.", u.ID, u.Tier, u.SubscribedUntil.String())
-	}
-	// Re-set the tier cache for this user, in case their tier changed.
-	api.staticUserTierCache.Set(u.Sub, u)
-	return err
 }
 
 // stripePrices returns a mapping of Stripe price ids to Skynet tiers.
